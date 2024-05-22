b4587bd014be036df395debfb1135eb5cd66cd5f
ingobecker <mail@ingobecker.de>
2024-05-22 10:55:38 +0200

    Build login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,40 @@
-
+import axios from "axios";
+import "./Login.css"
+import { FormEvent, useState } from "react";
 
 export default function App () {
-  return (
-    <div>
-      <h1>Dock Dash</h1>
-      <div className="card">
-        <button>
-          Login
-        </button>
-      </div>
-    </div>
-  );
+
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    function login(){
+        axios.post("/api/users/login", {}, {
+            auth:{
+                username: username,
+                password: password
+            }
+        })
+        .then(() => console.log("logged in"))
+        .catch((e) => console.log(e));
+    }
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        login();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <h1>Dock Dash</h1>
+            <div className={"login-wrapper"}>
+                <input placeholder={"Username"} value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <input placeholder={"Passswort"} type={"password"} value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div className="card">
+                <button>
+                    Login
+                </button>
+            </div>
+        </form>
+    );
 }
